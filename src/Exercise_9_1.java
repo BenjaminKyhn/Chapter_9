d5cd10316478d250756f804a7cfbdc1317ea58be
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-11-20 12:51:38 +0100

    Exercise 9.1 added

--- src/Exercise_9_1.java
+++ src/Exercise_9_1.java
@@ -2,6 +2,16 @@ public class Exercise_9_1 {
     public static void main(String[] args) {
         Rectangle rectangle1 = new Rectangle(4,40);
         Rectangle rectangle2 = new Rectangle(3.5, 35.9);
+
+        System.out.println("The width of rectangle1 is " + rectangle1.width);
+        System.out.println("The height of rectangle1 is " + rectangle1.height);
+        System.out.println("The area of rectangle1 is " + rectangle1.getArea());
+        System.out.println("The perimeter of rectangle1 is " + rectangle1.getPerimeter());
+
+        System.out.println("The width of rectangle2 is " + rectangle2.width);
+        System.out.println("The height of rectangle2 is " + rectangle2.height);
+        System.out.println("The area of rectangle2 is " + rectangle2.getArea());
+        System.out.println("The perimeter of rectangle2 is " + rectangle2.getPerimeter());
     }
 }
 
